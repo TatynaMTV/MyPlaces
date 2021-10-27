dd5de7bdae57e597ec0f4ed2deed8bd7d4be146d
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2021-10-27 23:53:47 +0300

    TableView CustomTableViewCell LaunchScreen

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Татьяна Мальчик on 27.10.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var typeLable: UILabel!
+    
+}
---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Татьяна Мальчик on 27.10.2021.
+//
+
+import UIKit
+
+class NewPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
