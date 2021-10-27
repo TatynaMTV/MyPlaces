a34ddffdb850fc005bde8f349d8e7b7aa909fe4a
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2021-10-27 23:10:44 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Татьяна Мальчик on 27.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
